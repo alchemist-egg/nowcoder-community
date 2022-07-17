4789917ad703559eb9d1385482d3706e13fc34d3
alchemist <alchemist_egg@163.com>
2022-07-17 16:11:56 +0800

    评论功能

---
+++ src/main/java/com/alchemist/nowcoder/controller/CommentController.java
@@ -0,0 +1,35 @@
+package com.alchemist.nowcoder.controller;
+
+import com.alchemist.nowcoder.entity.Comment;
+import com.alchemist.nowcoder.service.CommentService;
+import com.alchemist.nowcoder.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+
+    @RequestMapping(value = "/add/{discussPostId}", method = RequestMethod.POST)
+    public String addComment(@PathVariable("discussPostId") int discussPostId, Comment comment) {
+
+        comment.setUserId(hostHolder.getUser().getId());
+        comment.setCreateTime(new Date());
+        comment.setStatus(0);
+        commentService.addComment(comment);
+
+        return "redirect:/discuss/detail/" + discussPostId;
+    }
+}
--- src/main/java/com/alchemist/nowcoder/service/CommentService.java
+++ src/main/java/com/alchemist/nowcoder/service/CommentService.java
@@ -1,20 +1,30 @@
 package com.alchemist.nowcoder.service;
 
 import com.alchemist.nowcoder.dao.CommentMapper;
+import com.alchemist.nowcoder.dao.DiscussPostMapper;
 import com.alchemist.nowcoder.entity.Comment;
+import com.alchemist.nowcoder.util.CommunityConstant;
+import com.alchemist.nowcoder.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
 @Service
-public class CommentService {
+public class CommentService implements CommunityConstant {
 
     @Autowired
     private CommentMapper commentMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
 
     public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
         return commentMapper.selectCommentsByEntity(entityType, entityId, offset, limit);
@@ -26,6 +36,20 @@ public class CommentService {
 
     @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
     public int addComment(Comment comment) {
-        return commentMapper.insertComment(comment);
+        if (comment == null) {
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+
+        // 转义html,过滤敏感词
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        comment.setContent(sensitiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        if (comment.getEntityType() == ENTITY_TYPE_POST) {
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(), comment.getEntityId());
+            discussPostMapper.updateCommentCount(comment.getEntityId(), count);
+        }
+        return rows;
     }
+
 }
